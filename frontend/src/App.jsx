bb064dda17d98be6c56d0e822e22e0d14a0dadb3
OmTi-wari <omtiwari0211@gmail.com>
2025-10-01 00:42:02 +0530

    route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className = 'text-red-500 text-4xl'>
-     App
-    </div>
+    <>
+    <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+     </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
